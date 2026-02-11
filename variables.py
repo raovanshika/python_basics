72cbdc997f6bb52c58196e45497ec38bf2dbb201
Vanshika Rao <156182659+raovanshika@users.noreply.github.com>
2026-02-11 05:41:37 +0000

    add variables and input-output basics

---
+++ variables.py
@@ -0,0 +1,10 @@
+# Creating variables
+age = 19 #int data type
+height = 5.3 #float data type (decimal)
+name = "Vanshika" #char data type
+is_student = True #boolean data type 
+
+print(age)
+print(height)
+print(name)
+print(is_student)
