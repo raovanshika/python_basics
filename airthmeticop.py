66af23ae93177a032c6f39c7f395f37dc9f900e0
Vanshika Rao <156182659+raovanshika@users.noreply.github.com>
2026-02-11 05:48:33 +0000

    add airthmaticop and input-output basics

---
+++ airthmeticop.py
@@ -0,0 +1,8 @@
+a = 10 #assignmemt the value (int data type) in variable a
+b = 3
+
+print(a + b)   # addition
+print(a - b)   # subtraction
+print(a * b)   # multiplication
+print(a / b)   # division
+print(a % b)   # remainder
