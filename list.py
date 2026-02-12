bac73fbe67182a224a9200763ca54d4255f14152
Vanshika Rao <156182659+raovanshika@users.noreply.github.com>
2026-02-12 17:40:32 +0000

    add list and input-output basics

---
+++ list.py
@@ -0,0 +1,7 @@
+marks = [85, 90, 78, 92]
+
+print(marks)
+print(marks[0])   # first element
+print(marks[-1])  # last element
+#negative indexing start from the end of the list 
+# 0 is th e begnning of list 
