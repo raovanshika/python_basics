efcf13086787ab7db4b8c47dce6da5cf6490438a
Vanshika Rao <156182659+raovanshika@users.noreply.github.com>
2026-02-12 17:52:21 +0000

    add even_odd aand inout-output basics

---
+++ even_odd.py
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3, 4, 5, 6] #list
+for num in numbers: #loop through each number in the list
+    if num % 2 == 0: #check if the number is even
+        print(num, "is even")
+    else:
+        print(num, "is odd")
