bccfddf62219b3eddd399d1ec380520665debddb
Vanshika Rao <156182659+raovanshika@users.noreply.github.com>
2026-02-12 10:56:14 +0000

    add if_else and input-output basics

---
+++ if_else.py
@@ -0,0 +1,6 @@
+# age = 18 #this if then value of age is predefined 
+age = int(input("Enter your age: ")) #this if the user enter the value of age
+if age >= 18: #checks if the value of age is greater than or equal to 18
+    print("You are eligible to vote") #if yes this is executed
+else:
+    print("You are not eligible to vote") #if not then this is executed.
