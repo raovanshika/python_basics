4f64c0c6d5da8480f28ee52effa4109f1be78797
Vanshika Rao <156182659+raovanshika@users.noreply.github.com>
2026-02-12 17:47:48 +0000

    add loops and input-output basics

---
+++ loops.py
@@ -0,0 +1,9 @@
+#LOOPS , LIST , IF-ELSE Combined
+numbers = [1, 2, 3, 4, 5] #list
+
+# for num in numbers: #wil run till the condition is satisfied
+  
+for num in numbers:
+    if num>3:
+      print(num)
+else : print("Loop is completed") #this will be executed after the loop is completed, it is not part of the loop.
