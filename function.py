b80637bf737e77aaa6b2b35bbb2f6942cffc6a78
Vanshika Rao <156182659+raovanshika@users.noreply.github.com>
2026-02-12 17:57:11 +0000

    add function and input-output basics

---
+++ function.py
@@ -0,0 +1,5 @@
+def add(a, b):#function definition ( Function avoids code repetition)
+    return a + b #function body
+
+result = add(5, 3) #function call, we are passing 5 and 3 as arguments to the function add, and the result will be stored in the variable result
+print(result) #output will be 8, because the function add will return the sum of 5 and 3 which is 8.
