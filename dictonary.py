ff077dd2c9bb016bb4496f53762401edce6ce94c
Vanshika Rao <156182659+raovanshika@users.noreply.github.com>
2026-02-12 17:55:14 +0000

    add dictonary and input-output basics

---
+++ dictonary.py
@@ -0,0 +1,8 @@
+student = { #dictionary
+    "name": "Amit", #key : value pair
+    "age": 20,
+    "course": "CSE"
+}
+
+print(student["name"]) #to access the value of a key in a dictionary we use the key inside square brackets
+print(student["age"])
